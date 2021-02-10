b074f05bf230b93e16f03d929bea1c734f004653
Alex Reinking <alex.reinking@gmail.com>
2021-02-10 04:05:14 -0800

    Fix build, use AOT generators.

--- spook.cpp
+++ spook.cpp
@@ -2,37 +2,43 @@
 // We load an in image and call the pipeline in a loop.
 // TODO Check for completion flags
 
-#include "skeletonide.h"
-#include "halide_target_check.h"
+#include "HalideBuffer.h"
 #include "halide_image_io.h"
-#include <stdio.h>
+#include "skeletonide.h"
 #include <chrono>
+#include <cstdio>
 
 #define ITERS 100
 
 using namespace Halide;
 using namespace Halide::Tools;
 
-int main(int argc, char **argv){
-    Target target = find_gpu_target();
-    if (!target.has_gpu_feature()) {
-        return -1;
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: %s <input> <output>\n", argv[0]);
+        return 1;
     }
-    Runtime::Buffer<uint8_t> input = Tools::load_and_convert_image("test/images/tiled_horse.png");
+
+    Runtime::Buffer<uint8_t> input = Tools::load_and_convert_image(argv[1]);
     Runtime::Buffer<uint8_t> output(input.width(), input.height());
     auto orig_input = input;
+
     auto t1 = std::chrono::high_resolution_clock::now();
-    for(int j=0; j<ITERS; j++){
-        for(int i=0; i<29; i++){ // TODO remove this hardcoded limit.
+
+    for (int j = 0; j < ITERS; j++) {
+        for (int i = 0; i < 29; i++) {// TODO remove this hardcoded limit.
             skel(input, output);
             input = output;
         }
         input = orig_input;
     }
+
     auto t2 = std::chrono::high_resolution_clock::now();
     auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-    printf("Mean time to skeletonize: %fms\n", dur /(float)ITERS);
-    Tools::convert_and_save_image(output, "spook_out.png");
+    printf("Mean time to skeletonize: %fms\n", dur / (float) ITERS);
+
+    Tools::convert_and_save_image(output, argv[2]);
     printf("Image saved\n");
+
     return 0;
 }
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,87 +1,79 @@
 #include "Halide.h"
-#include "halide_target_check.h"
-#include "halide_image_io.h"
-
 using namespace Halide;
 
 // The pipeline below is a single iteration of Zhang-Suen algorithm.
-int main(int argc, char **argv) {
-    Target target = find_gpu_target();
-    if (!target.has_gpu_feature()) {
-        return false;
+class Skeletonide : public Generator<Skeletonide> {
+public:
+    Input<Buffer<uint8_t>> input{"input", 2};
+    Output<Buffer<uint8_t>> skel2{"skel2", 2};
+
+    void generate() {
+        Expr W = input.width();
+        Expr H = input.height();
+
+        // offsets of neighbours of a given pixel (x, y)
+        int n_x_idx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int n_y_idx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+        Buffer<int> n_x_idx_buf(n_x_idx);
+        Buffer<int> n_y_idx_buf(n_y_idx);
+
+        Var x, y, k;
+        Func in_bounded = BoundaryConditions::repeat_edge(input);
+
+        // fn to get the nth neighbours  of a pixel
+        Func nbr;
+        nbr(x, y, k) = in_bounded(x + n_x_idx_buf(k), y + n_y_idx_buf(k));
+
+        // fn to count the number of non-zero neighbours
+        RDom j(0, 8);
+        Func nbr_cnt("nbr_cnt");
+        nbr_cnt(x, y) = sum(select(nbr(x, y, j) > 0, 1, 0));
+
+        // fn to count the number of transitions
+        Func zero2one("zero2one");
+        RDom t(0, 8);
+        Expr zero2one_cond = nbr(x, y, t % 8) == 0 && nbr(x, y, (t + 1) % 8) == 255;
+        zero2one(x, y) = sum(select(zero2one_cond, 1, 0));
+
+        // step-1 of Zhang-Suen method
+        Expr fst_cnd;
+        fst_cnd = (nbr_cnt(x, y) >= 2 && nbr_cnt(x, y) <= 6 && zero2one(x, y) == 1 &&
+                   nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 4) == 0 &&
+                   nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 6) == 0);
+
+        Func skel1("skel1");
+        skel1(x, y) = cast<uint8_t>(select(fst_cnd, 255, in_bounded(x, y)));
+        //    skel1.compute_root().vectorize(x, 8).parallel(y);
+
+        Var c, i, block, thread, x0, y0, xi, yi;
+        skel1.compute_root().gpu_tile(x, y, x0, y0, xi, yi, 8, 8);
+
+        // step-2 of Zhang-Suen method
+        // operate on the array modified in step-1
+        Func nbr2;
+        nbr2(x, y, k) =
+                skel1(clamp(x + n_x_idx_buf(k), 0, W), clamp(y + n_y_idx_buf(k), 0, H));
+
+        RDom j2(0, 8);
+        Func nbr_cnt2("nbr_cnt2");
+        nbr_cnt2(x, y) = sum(select(nbr2(x, y, j2) > 0, 1, 0));
+
+        Func zero2one2("zero2one2");
+        RDom t2(0, 8);
+        Expr zero2one_cond2 =
+                nbr2(x, y, t2 % 8) == 0 && nbr2(x, y, (t2 + 1) % 8) == 255;
+        zero2one2(x, y) = sum(select(zero2one_cond2, 1, 0));
+
+        Expr snd_cnd;
+        snd_cnd =
+                (nbr_cnt2(x, y) >= 2 && nbr_cnt2(x, y) <= 6 && zero2one2(x, y) == 1 &&
+                 nbr2(x, y, 0) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0 &&
+                 nbr2(x, y, 2) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0);
+
+        skel2(x, y) = cast<uint8_t>(select(snd_cnd, 255, skel1(x, y)));
+        //    skel2.vectorize(x, 8).parallel(y);
+        skel2.gpu_tile(x, y, x0, y0, xi, yi, 8, 8);
     }
-    ImageParam input(type_of<uint8_t>(), 2);
-    Expr W = input.width();
-    Expr H = input.height();
+};
 
-    Var x_outer, x_inner, y_outer, y_inner, tile_index;
-
-    // offsets of neighbours of a given pixel (x, y)
-    int n_x_idx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
-    int n_y_idx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-    Buffer<int> n_x_idx_buf(n_x_idx);
-    Buffer<int> n_y_idx_buf(n_y_idx);
-
-    Var x, y, k;
-    Func in_bounded = BoundaryConditions::repeat_edge(input);
-
-    // fn to get the nth neighbours  of a pixel
-    Func nbr;
-    nbr(x, y, k) = in_bounded(x + n_x_idx_buf(k), y + n_y_idx_buf(k));
-
-    // fn to count the number of non-zero neighbours
-    RDom j(0, 8);
-    Func nbr_cnt("nbr_cnt");
-    nbr_cnt(x, y) = sum(select(nbr(x, y, j) > 0, 1, 0));
-
-    // fn to count the number of transitions
-    Func zero2one("zero2one");
-    RDom t(0, 8);
-    Expr zero2one_cond = nbr(x, y, t % 8) == 0 && nbr(x, y, (t + 1) % 8) == 255;
-    zero2one(x, y) = sum(select(zero2one_cond, 1, 0));
-
-    // step-1 of Zhang-Suen method
-    Expr fst_cnd;
-    fst_cnd = (nbr_cnt(x, y) >= 2 && nbr_cnt(x, y) <= 6 && zero2one(x, y) == 1 &&
-               nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 4) == 0 &&
-               nbr(x, y, 0) * nbr(x, y, 2) * nbr(x, y, 6) == 0);
-
-    Func skel1("skel1");
-    skel1(x, y) = cast<uint8_t>(select(fst_cnd, 255, in_bounded(x, y)));
-//    skel1.compute_root().vectorize(x, 8).parallel(y);
-
-    Var c, i, block, thread, x0, y0, xi, yi;
-    skel1.compute_root().gpu_tile(x, y, x0, y0, xi, yi, 8, 8);
-
-    // step-2 of Zhang-Suen method
-    // operate on the array modified in step-1
-    Func nbr2;
-    nbr2(x, y, k) =
-            skel1(clamp(x + n_x_idx_buf(k), 0, W), clamp(y + n_y_idx_buf(k), 0, H));
-
-    RDom j2(0, 8);
-    Func nbr_cnt2("nbr_cnt2");
-    nbr_cnt2(x, y) = sum(select(nbr2(x, y, j2) > 0, 1, 0));
-
-    Func zero2one2("zero2one2");
-    RDom t2(0, 8);
-    Expr zero2one_cond2 =
-            nbr2(x, y, t2 % 8) == 0 && nbr2(x, y, (t2 + 1) % 8) == 255;
-    zero2one2(x, y) = sum(select(zero2one_cond2, 1, 0));
-
-    Expr snd_cnd;
-    snd_cnd =
-            (nbr_cnt2(x, y) >= 2 && nbr_cnt2(x, y) <= 6 && zero2one2(x, y) == 1 &&
-             nbr2(x, y, 0) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0 &&
-             nbr2(x, y, 2) * nbr2(x, y, 4) * nbr2(x, y, 6) == 0);
-
-    Func skel2("skel2");
-    skel2(x, y) = cast<uint8_t>(select(snd_cnd, 255, skel1(x, y)));
-//    skel2.vectorize(x, 8).parallel(y);
-    skel2.gpu_tile(x, y, x0, y0, xi, yi, 8, 8);
-
-    // Compiling to a static lib
-    skel2.compile_to_static_library("skeletonide", {input}, "skel", target);
-
-    return 0;
-}
+HALIDE_REGISTER_GENERATOR(Skeletonide, skeletonide);
